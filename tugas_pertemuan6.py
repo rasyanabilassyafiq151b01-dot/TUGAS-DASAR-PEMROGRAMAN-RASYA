0bfdd688485c31320f92fa6136a070089eed99ed
Rasya Nabil <rasyanabilassyafiq151b01@gmail.com>
2025-11-02 01:39:59 +0700

    TUGAS PERTEMUAN 6

---
+++ tugas_pertemuan6.py
@@ -0,0 +1,12 @@
+L = [[0,1,2,3], [4,5,6,7], [8,9,10,11]]
+
+print("\nBaris Pertama, Kolom Pertama Adalah:")
+print(L[0][1])
+print("\nBaris Pertama, Kolom Kedua Adalah:")
+print(L[0][2])
+print("\nBaris Pertama, Kolom Ketiga Adalah:")
+print(L[0][3])
+print("\nBaris Ketiga, Kolom Ketiga Adalah:")
+print(L[2][1])
+print("\nBaris Keempat, Kolom Pertama Adalah:")
+print(L[0][0])
